0a0fef9244e58fd0729342eba40ebb2095d66a28
Marko Mušica <markomusica@gmail.com>
2022-06-23 16:12:09 +0200

    Kreiranje forme za registraciju

---
+++ Software/PC for you/PC for you/PC for you/RegistracijaForm.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace PC_for_you
+{
+    partial class RegistracijaForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRegistrirajSe = new System.Windows.Forms.Button();
+            this.textBoxLozinka = new System.Windows.Forms.TextBox();
+            this.textBoxKorime = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxPonovljenaLozinka = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxImePrezime = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 202);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Lozinka:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Korisničko ime:";
+            // 
+            // btnRegistrirajSe
+            // 
+            this.btnRegistrirajSe.Location = new System.Drawing.Point(118, 305);
+            this.btnRegistrirajSe.Name = "btnRegistrirajSe";
+            this.btnRegistrirajSe.Size = new System.Drawing.Size(282, 28);
+            this.btnRegistrirajSe.TabIndex = 9;
+            this.btnRegistrirajSe.Text = "Registriraj se";
+            this.btnRegistrirajSe.UseVisualStyleBackColor = true;
+            // 
+            // textBoxLozinka
+            // 
+            this.textBoxLozinka.Location = new System.Drawing.Point(118, 199);
+            this.textBoxLozinka.Name = "textBoxLozinka";
+            this.textBoxLozinka.Size = new System.Drawing.Size(282, 20);
+            this.textBoxLozinka.TabIndex = 8;
+            // 
+            // textBoxKorime
+            // 
+            this.textBoxKorime.Location = new System.Drawing.Point(118, 151);
+            this.textBoxKorime.Name = "textBoxKorime";
+            this.textBoxKorime.Size = new System.Drawing.Size(282, 20);
+            this.textBoxKorime.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 251);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Ponovi lozinku:";
+            // 
+            // textBoxPonovljenaLozinka
+            // 
+            this.textBoxPonovljenaLozinka.Location = new System.Drawing.Point(118, 247);
+            this.textBoxPonovljenaLozinka.Name = "textBoxPonovljenaLozinka";
+            this.textBoxPonovljenaLozinka.Size = new System.Drawing.Size(282, 20);
+            this.textBoxPonovljenaLozinka.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Ime i prezime:";
+            // 
+            // textBoxImePrezime
+            // 
+            this.textBoxImePrezime.Location = new System.Drawing.Point(118, 55);
+            this.textBoxImePrezime.Name = "textBoxImePrezime";
+            this.textBoxImePrezime.Size = new System.Drawing.Size(282, 20);
+            this.textBoxImePrezime.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(50, 104);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Adresa:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(118, 103);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(282, 20);
+            this.textBox1.TabIndex = 16;
+            // 
+            // RegistracijaForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 380);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxImePrezime);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxPonovljenaLozinka);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRegistrirajSe);
+            this.Controls.Add(this.textBoxLozinka);
+            this.Controls.Add(this.textBoxKorime);
+            this.Name = "RegistracijaForm";
+            this.Text = "Registracija";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRegistrirajSe;
+        private System.Windows.Forms.TextBox textBoxLozinka;
+        private System.Windows.Forms.TextBox textBoxKorime;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxPonovljenaLozinka;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxImePrezime;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
