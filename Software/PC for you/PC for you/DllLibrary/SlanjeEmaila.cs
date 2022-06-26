5a706baa123b4517c0e974bb66418803303d471c
Antonio Mohorić <mohoricantonio@gmail.com>
2022-06-26 20:17:58 +0200

    dodana klasa Slanje maila u DLL

--- Software/PC for you/PC for you/DllLibrary/SlanjeEmaila.cs
+++ Software/PC for you/PC for you/DllLibrary/SlanjeEmaila.cs
@@ -1,15 +1,7 @@
-﻿using FluentEmail.Smtp;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Mail;
-using System.Text;
-using System.Threading.Tasks;
-using FluentEmail.Core;
-using System.Web;
-using System.Windows.Forms;
+﻿using System.Net.Mail;
 using System.Net;
 using System.IO;
+using System.Windows.Forms;
 
 namespace PC_for_you
 {
